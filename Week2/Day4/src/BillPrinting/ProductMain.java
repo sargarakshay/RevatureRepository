9701975ea5b11097b7b0be0193305029adf70d4a
“Akshay <akshay.sargar17@vit.edu>
2021-08-05 19:29:24 +0530

    Adding Week2 Day4

---
+++ Week2/Day4/src/BillPrinting/ProductMain.java
@@ -0,0 +1,52 @@
+package BillPrinting;
+// Task 2 - Calculate total and print bill.
+//          Item Name          Price    Qty.    Line Total
+//          Line Total = Price * Qty.
+//          Bill Total = Total of Line Total + 5% tax;
+//          Items needs to be sorted based on Item Name.
+
+import java.util.*;
+
+public class ProductMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Product> productList = new ArrayList<>();
+		productList.add(new Product(1, "Bread", 19.99, 3));
+		productList.add(new Product(2, "Milk", 19.99, 5));
+		productList.add(new Product(3, "Beer", 229.99, 6));
+		productList.add(new Product(4, "Chicken", 129.99, 1));
+		productList.add(new Product(5, "Nuts", 599.99, 2));
+		productList.add(new Product(6, "Tea", 399.99, 9));
+		productList.add(new Product(7, "Sweets", 239.99, 1));
+		productList.add(new Product(8, "Fish", 449.99, 10));
+
+		Collections.sort(productList, (Product p1, Product p2) -> {
+			String s1 = p1.getName();
+			String s2 = p2.getName();
+			return s1.compareTo(s2);
+		});
+
+		ProductMain pm = new ProductMain();
+		pm.printMainProducts(productList);
+	}
+
+	public void printMainProducts(List<Product> productList) {
+		System.out.println("---------------------------------------");
+		System.out.println("#  Name    Price   Qty.  Total");
+		System.out.println("---------------------------------------");
+		int  i = 1;
+		for(Product p: productList) {
+			Product.printProducts(i++, p);
+		}
+		System.out.println("---------------------------------------");
+		Double totalBill = productList.stream().map(e -> e.getCost()*e.getQuantity()).reduce(0.0, (sum, bill) -> sum + bill);
+		
+		System.out.println("Total                    " + totalBill);
+		System.out.println("5% GST                   " + totalBill*0.05);
+		System.out.println("---------------------------------------");
+		System.out.println("Final Bill               " + (totalBill*1.05));
+		System.out.println("---------------------------------------");
+	}
+}
---
+++ Week2/Day4/src/StreamMinCost/BusMain.java
@@ -0,0 +1,38 @@
+package StreamMinCost;
+/*
+ * 	// Task 1 - Find the minimum price in 'Xyz Travels'
+	// Task 2 - Calculate total and print bill.
+	//          Item Name          Price    Qty.    Line Total
+	//          Line Total = Price * Qty.
+	//          Bill Total = Total of Line Total + 5% tax;
+	//          Items needs to be sorted based on Item Name.
+	// Task 3 - allMatch(), anyMatch(), distinct()
+	// Task 4 - Convert the Stream to Map.
+ */
+
+
+import java.util.*;
+
+public class BusMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Bus> busList = new ArrayList<>();
+		
+		busList.add(new Bus(100, "Abc Travels", 1200.22, 2.3f));
+		busList.add(new Bus(101, "Abc Travels", 1100.22, 2.4f));
+		busList.add(new Bus(190, "Xyz Travels", 1100.22, 2.3f));
+		busList.add(new Bus(200, "Jee Travels", 1000.22, 4.3f));
+		busList.add(new Bus(104, "Java Travels", 900.22, 4.3f));
+		busList.add(new Bus(111, "Xyz Travels", 1400.22, 4.3f));
+		
+		double minPrice = busList.stream().filter(e -> e.getProviderName().equals("Xyz Travels")).map(e -> e.getCost()).reduce(Double.MAX_VALUE, (min, price) -> Math.min(min, price));
+		double minPrice2 = busList.stream().filter(e -> e.getProviderName().equals("Xyz Travels")).map(e -> e.getCost()).min(Double::compare).get();
+
+		System.out.println("Minimum price : " +minPrice);
+		System.out.println("Minimum price : " +minPrice2);
+		
+	}
+
+	
+}
