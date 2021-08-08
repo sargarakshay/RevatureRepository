bdae0d3fb3c190499b202292a6f0947034bcbcc2
“Akshay <akshay.sargar17@vit.edu>
2021-08-08 13:17:50 +0530

    Updated and Added Week2 Day 4 & Day 5

---
+++ Week2/Day5/src/test/java/JUnitTestingDemo/JUnitTestingDemoTest.java
@@ -0,0 +1,40 @@
+package JUnitTestingDemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JUnitTestingDemoTest extends JUnitTestingDemo{
+
+    @Test
+    void maxElementInArray() {
+        assertEquals(43 ,maxElementInArray(new int[]{10, 30, 43, 12, 34, 12, 31}));
+    }
+
+    @Test
+    void cubeOfElement() {
+        try {
+            cubeOfElement(10);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void reverseStringWithEmptyString() {
+        try {
+            reverseString("");
+        } catch (InvalidStringException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void reverseStringWithString() {
+        try {
+            reverseString("Hello");
+        }  catch (InvalidStringException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
